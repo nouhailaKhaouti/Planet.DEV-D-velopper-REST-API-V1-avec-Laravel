14aaf1656e973597ed388ed066745b91ae5b8c04
Ghita <bahajghita3@gmail.com>
2023-02-23 10:33:16 +0100

    my part

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+  Route::resource('category',CategoryController::class);
+  Route::resource('comment',CommentController::class);
+
+
+
