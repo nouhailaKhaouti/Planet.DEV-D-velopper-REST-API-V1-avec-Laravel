57812267c9403be25180f9fe36b0235ec5dc480d
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-02-23 12:02:11 +0100

    change

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -17,7 +17,7 @@ class CategoryController extends Controller
     public function index()
     {
         $category = Category::all();
-        return response()->json(['response'=>'success','categories'=>$category]);
+        return response()->json(['response'=>'success','categories'=>$category],200);
     }
 
     /**
