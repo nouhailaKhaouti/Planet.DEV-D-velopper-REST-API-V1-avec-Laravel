687a5084f25790f79465151f211c00a473be1f38
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-02-24 10:17:35 +0100

    add authentication

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,6 +11,11 @@ use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /* Display a listing of the resource.
      */
     public function index()
